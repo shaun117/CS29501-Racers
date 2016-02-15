51f92db3fbf2dabbe4d0d9116f4b1909bee428b1
Matt Jasaitis <Matt@Matts-MacBook-Pro.local>
2016-02-15 15:46:51 -0600

    basic UI

---
+++ Racers/Assets/UIManager.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function OpenStart() {
+	Application.LoadLevel("StartScene");
+	}
+
+function loadRace() {
+	Application.LoadLevel("Race Scene");
+	}
+
+function loadSettings() {
+	Application.LoadLevel("SettingsScene");
+	}
+
+function loadCampaign() {
+	Application.LoadLevel("CampaignScene");
+	}
