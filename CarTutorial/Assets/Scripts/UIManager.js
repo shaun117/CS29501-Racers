8352250af220d27c2a33ccd960eba7e486a74cc3
Mataire <mataires@grinnell.edu>
2016-04-21 19:20:57 -0500

    Set stats foundation

--- CarTutorial/Assets/Scripts/UIManager.js
+++ CarTutorial/Assets/Scripts/UIManager.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 
+
 function OpenStart() {
 	Application.LoadLevel("StartScene");
 	}
--- CarTutorial/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ CarTutorial/Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -52,11 +52,13 @@ namespace UnityStandardAssets.Vehicles.Car
         public float BrakeInput { get; private set; }
         public float CurrentSteerAngle{ get { return m_SteerAngle; }}
         public float CurrentSpeed{ get { return m_Rigidbody.velocity.magnitude*2.23693629f; }}
-        public float MaxSpeed{get { return m_Topspeed; }}
+        public float MaxSpeed{get { return m_Topspeed; }
+                               set { this.m_Topspeed = value; } }
         public float Revs { get; private set; }
         public float AccelInput { get; private set; }
 		public Text SpeedoText;
 
+  
         // Use this for initialization
         private void Start()
         {
